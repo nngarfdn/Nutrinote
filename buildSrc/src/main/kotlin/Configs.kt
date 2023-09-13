9949f8555605a6b02794a240a423deea0bac99c6
Arief Zuhri <ariefzuhri72@gmail.com>
2023-09-13 10:10:01 +0700

    Bump version name from 0.5.0 to 0.5.1

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -5,8 +5,8 @@ object Configs {
     const val MIN_SDK = 21
     const val TARGET_SDK = 33
 
-    const val VERSION_CODE = 14
-    const val VERSION_NAME = "0.5.0"
+    const val VERSION_CODE = 15
+    const val VERSION_NAME = "0.5.1"
 
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 }
